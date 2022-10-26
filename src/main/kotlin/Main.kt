cf5162e99beadb551351126f8b207374ea16e72f
Bruno Coronado <brunocoronado@Braunybrunista666.local>
2022-10-26 17:27:25 -0500

    Tipos de datos en kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,28 @@
+/// Funcion main que es la principal, el punto de entrada
+/// para el proyecto.
+
+/// Constantes, se declaran en tiempo de compilación
+const val PI = 3.1416
+
 fun main(args: Array<String>) {
-    println("Hola mundo!")
+    /// Varibale
+    var name: String = "Bruno"
+    println(name)
+    name = "Francisco"
+    println(name)
+
+    /// Variable de solo lectura
+    val lastName: String = "Rangel"
+    println(lastName)
+
+    println(PI)
+
+    /// Tipos de datos
+    var numero: Int = 25
+    println(numero)
+    var nombre: String = "Hola Mundo"
+    println(numero)
+    var mentira: Boolean = true
+    println(numero)
+
 }
